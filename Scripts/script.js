5dbfa313288efda25b681d0cf074da56ce50f392
Michele Solimando <michelesolimando333@gmail.com>
2024-05-08 17:32:22 +0200

    added with javascript ease of the menu and a few style changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -6,11 +6,28 @@ menuButton.addEventListener("click" , e => {
     console.log(menuState)
 
     if (menuState === "open"){
-        menu.style.display = "none";
-        menu.setAttribute("state", "closed")
+        closeMenu();
+        // menu.style.display = "none";
+        // menu.setAttribute("state", "closed")
         
     }else if(menuState === "closed"){
-        menu.style.display = "flex";
-        menu.setAttribute("state", "open")
+        openMenu();
+        // menu.style.display = "flex";
+        // menu.setAttribute("state", "open")
     }
 })
+
+
+function openMenu(){
+    menu.setAttribute("state", "open")
+
+    menu.style.transitionTimingFunction = "ease";
+    menu.style.translate = "110svw";
+}
+
+function closeMenu(){
+    menu.setAttribute("state", "closed")
+    
+    menu.style.transitionTimingFunction = "ease-in";
+    menu.style.translate = "-110svw";
+}
