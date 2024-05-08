21fbe57e3292844c715c45d3bcaf2a237a59dd4e
Michele Solimando <michelesolimando333@gmail.com>
2024-05-08 21:52:30 +0200

    compleeted the animation of the menu button

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,37 @@
-let root = document.getRootNode();
-console.log(root);
-
 let menuButton = document.getElementById("menu-button");
 let menuButtonBars = menuButton.children;
 let menu = document.getElementById("sticky-menu");
+
+
+function openMenu(){
+    menu.setAttribute("state", "open")
+
+    menu.style.transitionTimingFunction = "ease";
+    menu.style.translate = "110svw";
+
+    menuButtonBars[0].style.rotate = "45deg";
+    menuButtonBars[0].style.translate = "0 8.5px";
+    menuButtonBars[1].style.transitionDuration = "0.3s"; 
+    menuButtonBars[1].style.backgroundColor = "rgba(0, 0, 0, 0)"; 
+    menuButtonBars[2].style.rotate = "-45deg";
+    menuButtonBars[2].style.translate = "0 -8.5px";
+}
+
+function closeMenu(){
+    menu.setAttribute("state", "closed")
+    
+    menu.style.transitionTimingFunction = "ease-in";
+    menu.style.translate = "-110svw";
+    
+    menuButtonBars[0].style.rotate = "0deg";
+    menuButtonBars[0].style.translate = "0 0px";
+    menuButtonBars[1].style.transitionDuration = "0.6s"; 
+    menuButtonBars[1].style.backgroundColor = "rgba(255, 255, 255, 255)"; 
+    menuButtonBars[2].style.rotate = "0deg";
+    menuButtonBars[2].style.translate = "0 0";
+}
+
+
 
 menuButton.addEventListener("click" , e => {
     let menuState = menu.getAttribute("state");
@@ -21,29 +49,3 @@ menuButton.addEventListener("click" , e => {
     }
 })
 
-
-function openMenu(){
-    menu.setAttribute("state", "open")
-
-    menu.style.transitionTimingFunction = "ease";
-    menu.style.translate = "110svw";
-
-    menuButtonBars[0].style.rotate = "45deg";
-    menuButtonBars[0].style.translate = "0 8.5px";
-    menuButtonBars[1].style.visibility = "hidden"; // Todo : get the root variable for the background color
-    menuButtonBars[2].style.rotate = "-45deg";
-    menuButtonBars[2].style.translate = "0 -8.5px";
-}
-
-function closeMenu(){
-    menu.setAttribute("state", "closed")
-    
-    menu.style.transitionTimingFunction = "ease-in";
-    menu.style.translate = "-110svw";
-    
-    menuButtonBars[0].style.rotate = "0deg";
-    menuButtonBars[0].style.translate = "0 0px";
-    menuButtonBars[1].style.visibility = "visible"; // Todo : get the root variable for the background color
-    menuButtonBars[2].style.rotate = "0deg";
-    menuButtonBars[2].style.translate = "0 0";
-}
