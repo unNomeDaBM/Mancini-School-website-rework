fea1307392057e759753735fe8a25de3cbd26597
Michele Solimando <michelesolimando333@gmail.com>
2024-06-02 15:55:43 +0200

    media scale compleeted, fixed js code for sliding and some other bugfix in css

--- Scripts/bacheca_slide.js
+++ Scripts/bacheca_slide.js
@@ -1,27 +1,32 @@
+const bacheca_width = document.getElementById("bacheca").offsetWidth;
 const bacheca_nav = document.getElementById("sliding nav");
 const bacheca_left_arrow = document.getElementById("bacheca left arrow");
 const bacheca_right_arrow = document.getElementById("bacheca right arrow");
 const bacheca_items_width = 280;
 const bacheca_items_gap = 40;
+const total_width = (bacheca_items_gap + bacheca_items_width)*7
+const number_of_items_that_fit = Math.round(bacheca_width/(bacheca_items_gap + bacheca_items_width) - 0.4);
 
 
 let bacheca_translate_x = 0;
-let bacheca_focus_element = 1;
-console.log(bacheca_nav.children.length)
 
 bacheca_left_arrow.addEventListener("click", ()=>{
     
-    if(bacheca_focus_element > 1){
+    if(Math.abs(bacheca_translate_x) >= bacheca_items_width + bacheca_items_gap){
         bacheca_nav.style.transform = `translateX(${bacheca_translate_x + (bacheca_items_width  + bacheca_items_gap)}px)`;
         bacheca_translate_x += (bacheca_items_width  + bacheca_items_gap);
-        bacheca_focus_element -= 1;
     }
 });
 bacheca_right_arrow.addEventListener("click", ()=>{
-    
-    if(bacheca_focus_element < (bacheca_nav.children.length - Math.floor(window.innerWidth/(bacheca_items_gap + bacheca_items_width))+1)){
+
+    if(Math.abs(bacheca_translate_x) < (total_width - number_of_items_that_fit*(bacheca_items_gap + bacheca_items_width))){
         bacheca_nav.style.transform = `translateX(${bacheca_translate_x - (bacheca_items_width  + bacheca_items_gap)}px)`;
         bacheca_translate_x -= (bacheca_items_width  + bacheca_items_gap);
-        bacheca_focus_element += 1;
     }
+
+    // console.log(`bacheca_translate_x: ${bacheca_translate_x}`)
+    // console.log(`number_of_items_that_fit: ${number_of_items_that_fit}`)
+    // console.log(`bacheca_width/(bacheca_items_gap + bacheca_items_width): ${bacheca_width/(bacheca_items_gap + bacheca_items_width)}`)
+    // console.log(`bacheca_items_gap + bacheca_items_width: ${bacheca_items_gap + bacheca_items_width}`)
+    // console.log(`total_width - number_of_items_that_fit*(bacheca_items_gap + bacheca_items_width): ${total_width - number_of_items_that_fit*(bacheca_items_gap + bacheca_items_width)}`);
 });
